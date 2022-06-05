267734a6cb0cf3f28eea5f9e5dd13d4cc96d3833
jojainrage <alymbaevabdulla@gmail.com>
2022-06-05 21:20:09 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        // || (or) && (and) !(not)
+        int abc=3;
+        if(!(abc==3)){
+            System.out.println("j");
+        }
+    }
+}
